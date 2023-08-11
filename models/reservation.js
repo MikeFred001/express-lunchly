0730dd981c173e2bf748fba23b6e78ba7463e464
michael frederickson <mpfplus@gmail.com>
2023-08-11 12:35:59 -0600

    WIP: Just completed add reservation functionality

--- models/reservation.js
+++ models/reservation.js
@@ -22,6 +22,7 @@ class Reservation {
   getFormattedStartAt() {
     return moment(this.startAt).format("MMMM Do YYYY, h:mm a");
   }
+  // what is this??????
 
   /** given a customer id, find their reservations. */
 
@@ -38,6 +39,33 @@ class Reservation {
     );
 
     return results.rows.map(row => new Reservation(row));
+  }
+
+  async save() {
+    if (this.id === undefined){
+      const result = await db.query(`
+        INSERT INTO reservations (customer_id, start_at, num_guests, notes)
+        VALUES ($1, $2, $3, $4)
+        RETURNING id`,
+        [this.customerId, this.startAt, this.numGuests, this.notes]
+      );
+      this.id = result.rows[0].id;
+    } else {
+        await db.query(`
+        UPDATE reservations (customer_id, start_at, num_guests, notes)
+        SET customer_id=$1,
+            start_at=$2,
+            num_guests=$3,
+            notes=$4
+        WHERE id = $5`, [
+          this.customerId,
+          this.startAt,
+          this.numGuests,
+          this.notes,
+          this.id
+        ]
+      );
+    }
   }
 }
 
